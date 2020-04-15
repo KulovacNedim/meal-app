f7c40684335b9d4b189fd226c155c61b35ec9d05
Nedim Kulovac <nedim.kulovac@gmail.com>
2020-04-15 22:42:21 +0200

    Tab navigator created - two tabs: first containg stack navigator to meals and second containing stack navigator to favorites. styled and displeyed correctly. MealList component created to utilize function of listing meals and ensure reusibility

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -1,11 +1,23 @@
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
 import { Platform } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import React from 'react';
+import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs'
 
 import CategoriesScreen from '../screens/CategoriesScreen';
 import CategoryMealsScreen from '../screens/CategoryMealsScreen';
 import MealDetailsScreen from '../screens/MealDetailsScreen';
+import FavoritesScreen from '../screens/FavoritesScreen';
 import Colors from '../constants/Colors';
+
+const defaultStackNavOptions = {
+    headerStyle: {
+        backgroundColor: Platform.OS === 'android' ? Colors.primaryColor : ''
+    },
+    headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primaryColor
+};
 
 const MealsNavigator = createStackNavigator(
     {
@@ -16,12 +28,55 @@ const MealsNavigator = createStackNavigator(
         MealDetails: MealDetailsScreen
     },
     {
-        defaultNavigationOptions: {
-            headerStyle: {
-                backgroundColor: Platform.OS === 'android' ? Colors.primaryColor : ''
-            },
-            headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primaryColor
-        }
+        defaultNavigationOptions: defaultStackNavOptions
     });
 
-export default createAppContainer(MealsNavigator);
+const FavNavigator = createStackNavigator(
+    {
+        Favorites: FavoritesScreen,
+        MealDetails: MealDetailsScreen
+    },
+    {
+        defaultNavigationOptions: defaultStackNavOptions
+    }
+);
+
+const tabScreenConfig = {
+    Meals: {
+        screen: MealsNavigator,
+        navigationOptions: {
+            tabBarIcon: (tabInfo) => {
+                return <Ionicons name="ios-restaurant" size={25} color={tabInfo.tintColor} />;
+            },
+            tabBarColor: Colors.primaryColor
+        }
+    },
+    Favorites: {
+        screen: FavNavigator,
+        navigationOptions: {
+            tabBarLabel: 'Favorites!',
+            tabBarIcon: (tabInfo) => {
+                return <Ionicons name="ios-star" size={25} color={tabInfo.tintColor} />;
+            },
+            tabBarColor: Colors.accentColor
+        }
+    }
+};
+
+const MealsFavTabNavigator = Platform.OS = 'android' ?
+    createMaterialBottomTabNavigator(
+        tabScreenConfig,
+        {
+            activeTintColor: 'white',
+            shifting: true
+        })
+    :
+    createBottomTabNavigator(
+        tabScreenConfig,
+        {
+            tabBarOptions: {
+                activeTintColor: Colors.accentColor
+            }
+        });
+
+export default createAppContainer(MealsFavTabNavigator);
---
+++ screens/FavoritesScreen.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import MealList from '../components/MealList';
+
+import { MEALS } from '../data/dummy-data';
+
+const FavoritesScreen = props => {
+
+    const fevMeals = MEALS.filter(meal => meal.id === 'm1' || meal.id === 'm2');
+
+    return <MealList listData={fevMeals} navigation={props.navigation} />;
+};
+
+FavoritesScreen.navigationOptions = {
+    headerTitle: 'Your Favorites'
+};
+
+export default FavoritesScreen;
