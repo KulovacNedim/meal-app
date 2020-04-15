b9a725957cc625c9da3717d3217aa687a8feadf0
Nedim Kulovac <nedim.kulovac@gmail.com>
2020-04-15 12:39:13 +0200

    some dummy data added to use it as source. MealsNavigator configured to use default options. Category ID passing to MealsDetailsScreen component added. Colors constants created to ensure uniqe look across app

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -1,14 +1,27 @@
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
+import { Platform } from 'react-native';
 
 import CategoriesScreen from '../screens/CategoriesScreen';
 import CategoryMealsScreen from '../screens/CategoryMealsScreen';
 import MealDetailsScreen from '../screens/MealDetailsScreen';
+import Colors from '../constants/Colors';
 
-const MealsNavigator = createStackNavigator({
-    Categories: CategoriesScreen,
-    CategoryMeals: CategoryMealsScreen,
-    MealDetails: MealDetailsScreen
-});
+const MealsNavigator = createStackNavigator(
+    {
+        Categories: CategoriesScreen,
+        CategoryMeals: {
+            screen: CategoryMealsScreen
+        },
+        MealDetails: MealDetailsScreen
+    },
+    {
+        defaultNavigationOptions: {
+            headerStyle: {
+                backgroundColor: Platform.OS === 'android' ? Colors.primaryColor : ''
+            },
+            headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primaryColor
+        }
+    });
 
 export default createAppContainer(MealsNavigator);
--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,25 +1,37 @@
 import React from 'react';
 import { View, Text, Button, StyleSheet } from 'react-native';
 
+import { CATEGORIES } from '../data/dummy-data';
+
 const CategoryMealScreen = props => {
-    return (
-      <View style={styles.screen}>
-        <Text>The Category Meal Screen!</Text>
-        <Button title="Go to Details" onPress={() => {
-            props.navigation.navigate({
-                routeName: 'MealDetails'
-            });
-        }} />
-      </View>
-    );
+  const catId = props.navigation.getParam('categoryId');
+  const selectedCategory = CATEGORIES.find(cat => cat.id === catId);
+  return (
+    <View style={styles.screen}>
+      <Text>The Category Meal Screen! - {selectedCategory.title}</Text>
+      <Button title="Go to Details" onPress={() => {
+        props.navigation.navigate({
+          routeName: 'MealDetails'
+        });
+      }} />
+    </View>
+  );
+};
+
+CategoryMealScreen.navigationOptions = (navigationData) => {
+  const catId = navigationData.navigation.getParam('categoryId');
+  const selectedCategory = CATEGORIES.find(cat => cat.id === catId);
+  return {
+    headerTitle: selectedCategory.title
   };
-  
-  const styles = StyleSheet.create({
-    screen: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center'
-    }
-  });
-  
-  export default CategoryMealScreen;
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+});
+
+export default CategoryMealScreen;
