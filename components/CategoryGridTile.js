5c31c9085f7c04dea690c331c147290e8eb27776
Nedim Kulovac <nedim.kulovac@gmail.com>
2020-04-15 19:38:56 +0200

    Meal model created and dummy-data populeted with meals. Rendering meals and meals details imlemented

--- components/CategoryGridTile.js
+++ components/CategoryGridTile.js
@@ -35,16 +35,16 @@ const styles = StyleSheet.create({
         margin: 15,
         height: 150,
         borderRadius: 10,
-        overflow: 'hidden'
-    },
-    container: {
-        flex: 1,
-        borderRadius: 10,
+        overflow: Platform.OS == 'android' & Platform.Version >= 21 ? 'hidden' : 'visible',
+        elevation: 5,
         shadowColor: 'black',
         shadowOpacity: 0.26,
         shadowOffset: { width: 0, height: 2 },
         shadowRadius: 10,
-        elevation: 3,
+    },
+    container: {
+        flex: 1,
+        borderRadius: 10,
         padding: 15,
         justifyContent: 'flex-end',
         alignItems: 'flex-end'
