e9d249b112298ff9ff2ed805dc6a3ef85c8ed3c0
Nedim Kulovac <nedim.kulovac@gmail.com>
2020-04-16 12:07:09 +0200

    DefaultText.js created to wrap default text styling. MealDetailsScreen made to render details and FiltersScreen display filters. FilterSwitch component created as internal component inside od FilterScreen component to ensure reusability.

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -2,7 +2,7 @@ import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
 import { createDrawerNavigator } from 'react-navigation-drawer';
-import { Platform } from 'react-native';
+import { Text, Platform } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import React from 'react';
 import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs'
@@ -19,7 +19,13 @@ const defaultStackNavOptions = {
       backgroundColor: Platform.OS === 'android' ? Colors.primaryColor : ''
     },
     headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primaryColor,
-    headerTitle: 'A Screen'
+    headerTitle: 'A Screen',
+    headerTitleStyle: {
+      fontFamily: 'open-sans-bold'
+    },
+    headerBackTitleStyle: {
+      fontFamily: 'open-sans'
+    }
   };
   
   const MealsNavigator = createStackNavigator(
@@ -58,7 +64,8 @@ const defaultStackNavOptions = {
             <Ionicons name="ios-restaurant" size={25} color={tabInfo.tintColor} />
           );
         },
-        tabBarColor: Colors.primaryColor
+        tabBarColor: Colors.primaryColor,
+        tabBarLabel: Platform.OS === 'android' ? <Text style={{fontFamily: 'open-sans'}}>Meals</Text> : 'Meals'
       }
     },
     Favorites: {
@@ -67,7 +74,8 @@ const defaultStackNavOptions = {
         tabBarIcon: tabInfo => {
           return <Ionicons name="ios-star" size={25} color={tabInfo.tintColor} />;
         },
-        tabBarColor: Colors.accentColor
+        tabBarColor: Colors.accentColor,
+        tabBarLabel: Platform.OS === 'android' ? <Text style={{fontFamily: 'open-sans'}}>Favorites</Text> : 'Favorites'
       }
     }
   };
@@ -83,6 +91,9 @@ const defaultStackNavOptions = {
         })
       : createBottomTabNavigator(tabScreenConfig, {
           tabBarOptions: {
+            labelStyle: {
+              fontFamily: 'open-sans-bold'
+            },
             activeTintColor: Colors.accentColor
           }
         });
